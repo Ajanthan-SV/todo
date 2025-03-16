91c96c670868bb4e02c945be638a6fac7738f3f5
Ajanthan-SV <harissanthisurya@gmail.com>
2025-03-16 23:47:43 +0530

    navbar fix

--- src/views/Navbar.jsx
+++ src/views/Navbar.jsx
@@ -63,7 +63,7 @@ function Navbar() {
                   <Link className={`nav-link ${location.pathname === '/todo' ? 'active' : ''}`} to="/todo"><b>Todo</b></Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" onClick={handleLogout} style={{cursor:"pointer"}}><b>Logout</b> </a>
+                  <a className="nav-link" href='/' onClick={handleLogout} style={{cursor:"pointer"}}><b>Logout</b> </a>
                 </li>
               </>
               }   
